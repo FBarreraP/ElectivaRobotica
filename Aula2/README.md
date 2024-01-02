38c10570355cc6d4dafe41377d0fd66f3468c678
“FBarreraP” <fbarrera6@gmail.com>
2024-01-01 20:03:22 -0500

    \n input

--- Aula2/README.md
+++ Aula2/README.md
@@ -51,7 +51,7 @@ b = "Mundo"
 c = 2024.0
 
 print("Texto: %s %s %.2f" %(a,b,c))
-print("Texto:",a,b,c) 
+print("Texto:",a,b,c)  
 print("Texto: "+str(a)+' '+str(b)+' '+str(c))
 print(f"Texto: {a} {b} {c:.2f}")
 print("Texto: {0} {1} {2:.2f}".format(a,b,c))
@@ -230,8 +230,8 @@ def sum(a,b):
     c = a + b
     return c
 
-num1 = float(input('Ingrese el primer número: '))
-num2 = float(input('Ingrese el segundo número: '))
+num1 = float(input('Ingrese el primer número: \n'))
+num2 = float(input('Ingrese el segundo número: \n'))
 num3 = sum(num1,num2)
 print('%.2f + %.2f = %.2f' %(num1,num2,num3))
 ```
--- Aula2/prueba.py
+++ Aula2/prueba.py
@@ -1,9 +1,17 @@
-a = 'Hola'
-b = "Mundo"
-c = 2024.0
+def data():
+    global grades
+    for i in range(0,5,1):
+        n = float(input('Ingrese la nota '+str(i+1)+" : \n"))
+        grades.append(n)
 
-print("Texto: %s %s %.2f" %(a,b,c))
-print("Texto:",a,b,c) 
-print("Texto: "+str(a)+' '+str(b)+' '+str(c))
-print(f"Texto: {a} {b} {c:.2f}")
-print("Texto: {0} {1} {2:.2f}".format(a,b,c))
+def average(notas):
+    s = 0.0
+    for i in range(0,5,1):
+        s += notas[i]
+    a = s/len(notas)
+    return s,a
+
+grades = []
+data()
+r1,r2 = average(grades)
+print('La suma y el promedio de las notas son: %.2f y %.2f' %(r1,r2))
