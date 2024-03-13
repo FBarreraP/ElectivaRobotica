7b573c0310b019a38186ee3c18f054f824850763
FBarreraP <fbarrera6@gmail.com>
2024-03-13 16:11:37 -0500

    Actualización aula 13

--- Aula13/Prueba.py
+++ Aula13/Prueba.py
@@ -1,10 +1,11 @@
 from sympy import *
-from RotarZ import *
 import numpy
+# from roboticstoolbox import *
+from scipy.spatial.transform import Rotation as R
 
 #Transformaciones (MTH)
 
-syms h1 h2 theta1 theta2 l1 l2
+theta1, theta2, h1, h2, l1, l2 = symbols('theta1 theta2 h1 h2 l1 l2')
 # theta1 = pi/2
 # theta2 = pi/2
 # l1 = 5;
@@ -12,28 +13,53 @@ syms h1 h2 theta1 theta2 l1 l2
 # h1 = 3;
 # h2 = 2;
 
-T01 = [1 0 0 0;
-       0 1 0 0;
-       0 0 1 h1;
-       0 0 0 1]
+T01 = numpy.array([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, h1],
+                   [0, 0, 0, 1]])
 
-T12 = [cos(theta1) -sin(theta1) 0 l1*cos(theta1);
-       sin(theta1) cos(theta1)  0 l1*sin(theta1);
-       0           0            1 0;
-       0           0            0 1]
+T12 = numpy.array([[cos(theta1), -sin(theta1), 0, l1*cos(theta1)],
+                   [sin(theta1), cos(theta1), 0, l1*sin(theta1)],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]])
+
+T23 = numpy.array([[cos(theta2), -sin(theta2), 0, l2*cos(theta2)],
+                   [sin(theta2), cos(theta2), 0, l2*sin(theta2)],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]])
+
    
-T23 = [cos(theta2) -sin(theta2) 0 l2*cos(theta2);
-       sin(theta2) cos(theta2)  0 l2*sin(theta2);
-       0           0            1 0;
-       0           0            0 1]
-   
-T34 = [1 0 0 0;
-       0 1 0 0;
-       0 0 1 -h2;
-       0 0 0 1]
+T34 = numpy.array([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, -h2],
+                   [0, 0, 0, 1]])
 
-T04 = simplify(T01*T12*T23*T34)
-% T04 = T01*T12*T23*T34
+T02 = numpy.matmul(T01,T12)
+T24 = numpy.matmul(T23,T34)
+T04 = simplify(numpy.matmul(T02,T24))
+# T04 = (numpy.matmul(T02,T24))
+print(f'T04 = {T04}')
 
-% r = T04(1:3,1:3)
-% m = rad2deg(tr2rpy(r,'zyx'))
+# r = R.from_matrix(T04[:3,:3])
+# print(f'r = {r}')
+# # m = numpy.rad2deg(tr2rpy(r,'zyx'))
+# m = r.as_euler('zyx', degrees=True)
+# print(f'm = {m}')
+
+
+# import numpy as np
+# from scipy.spatial.transform import Rotation as R
+
+# # Definir una matriz de transformación homogénea (por ejemplo, rotación de 45 grados alrededor del eje z)
+# R_matrix = np.array([[np.sqrt(2)/2, -np.sqrt(2)/2, 0, 0],
+#                      [np.sqrt(2)/2,  np.sqrt(2)/2, 0, 0],
+#                      [          0,            0, 1, 0],
+#                      [          0,            0, 0, 1]])
+
+# # Convertir la matriz de transformación en un objeto de rotación
+# r = R.from_matrix(R_matrix[:3, :3])
+
+# # Obtener los ángulos de rollo, cabeceo y guiñada (RPY)
+# rpy = r.as_euler('zyx', degrees=True)
+
+# print("Angulos de RPY (grados):", rpy)
--- Aula13/README.md
+++ Aula13/README.md
@@ -122,6 +122,61 @@ $$𝑇_4^3 = \begin{bmatrix}
 0 & 0 & 0 & 1
 \end{bmatrix}$$
 
+Comprobación en `Python`
+
+```python
+from sympy import *
+import numpy
+# from roboticstoolbox import *
+from scipy.spatial.transform import Rotation as R
+
+#Transformaciones (MTH)
+
+theta1, theta2, h1, h2, l1, l2 = symbols('theta1 theta2 h1 h2 l1 l2')
+# theta1 = pi/2
+# theta2 = pi/2
+# l1 = 5;
+# l2 = 5;
+# h1 = 3;
+# h2 = 2;
+
+T01 = numpy.array([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, h1],
+                   [0, 0, 0, 1]])
+
+T12 = numpy.array([[cos(theta1), -sin(theta1), 0, l1*cos(theta1)],
+                   [sin(theta1), cos(theta1), 0, l1*sin(theta1)],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]])
+
+T23 = numpy.array([[cos(theta2), -sin(theta2), 0, l2*cos(theta2)],
+                   [sin(theta2), cos(theta2), 0, l2*sin(theta2)],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]])
+
+   
+T34 = numpy.array([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, -h2],
+                   [0, 0, 0, 1]])
+
+T02 = numpy.matmul(T01,T12)
+T24 = numpy.matmul(T23,T34)
+T04 = simplify(numpy.matmul(T02,T24))
+# T04 = (numpy.matmul(T02,T24))
+print(f'T04 = {T04}')
+
+# r = R.from_matrix(T04[:3,:3])
+# print(f'r = {r}')
+# # m = numpy.rad2deg(tr2rpy(r,'zyx'))
+# m = r.as_euler('zyx', degrees=True)
+# print(f'm = {m}')
+
+```
+
+Comprobación en `Matlab`
+
 ```matlab
 %Transformaciones (MTH)
 
